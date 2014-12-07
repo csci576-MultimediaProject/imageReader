dc543ad39991785d1e2f9c47d82745f38b13e121
bossmeng17 <bossmeng@Farn-CherntekiMacBook-Pro-3.local>
2014-12-06 22:19:09 -0800

    open folder

--- imageReader.java
+++ imageReader.java
@@ -12,75 +12,89 @@ import javax.swing.JLabel;
 
 public class imageReader {
 
-	/**
-	 * @param args
-	 */
 
-	//private int iter = 9; // Number of Dwt iteration
-
+	static int counter = 0; /* count how many files in a folder */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		String fileName = args[0];
+		// File representing the folder that you select using a FileChooser
+		final File dir = new File(args[0]);
 		int width = 352;
 		int height = 288;
-
-		System.out.println("Filename : " + fileName); // print filename
-
-		imageReader imr = new imageReader();
 		
+		imageReadertest imr = new imageReadertest();
 
-		try {
-			File file = new File(args[0]);
-			InputStream is = new FileInputStream(file);
-			
-			long len = file.length(); // Calculate input file size
-			System.out.println("File length : " + len); // Print input file size
-			double framelen = (len / (width * height * 3)); // Calculate input// file's frames
-			System.out.println("Numbers of frames : " + framelen); // Print// input// file's// frames
-			BufferedImage frameA[] = new BufferedImage[(int) framelen]; // Set// the// frame// array
-			
-			//System.out.println("-------------"+hit.getHistogram());
-
-			byte[] bytes = new byte[(int) len];
-
-			int offset = 0;
-			int numRead = 0;
-			while (offset < bytes.length
-					&& (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
-				offset += numRead;
-			}
-
-			for (int i = 0; i < framelen; i++) {
-				BufferedImage img = new BufferedImage(width, height,
-						BufferedImage.TYPE_INT_RGB);
-				int ind = 0;
-				for (int y = 0; y < height; y++) {
-					for (int x = 0; x < width; x++) {
-
-						byte a = 0;
-						byte r = bytes[i * width * height * 3 + ind];
-						byte g = bytes[i * width * height * 3 + ind + height
-								* width];
-						byte b = bytes[i * width * height * 3 + ind + height
-								* width * 2];
-
-						int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
-						img.setRGB(x, y, pix);
-						ind++;
+		// array of supported extensions (use a List if you prefer)
+		final String[] EXTENSIONS = new String[]{
+				"rgb" // and other formats you need
+		};
+    
+		// filter to identify images based on their extensions
+		final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+			@Override
+			public boolean accept(final File dir, final String name) {
+				for (final String ext : EXTENSIONS) {
+					if (name.endsWith("." + ext)) {
+						return (true);
 					}
 				}
-				frameA[i] = img;
+				return (false);
 			}
-			is.close(); // Close file read
-			imr.histogram(frameA[0]);
-			imr.display(frameA);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+		};
+
+        if (dir.isDirectory()) { // make sure it's a directory
+            for ( final File file : dir.listFiles(IMAGE_FILTER)) {
+            	counter++; 
+                
+            	try {
+                	InputStream is = new FileInputStream(file);
+                	
+                	long len = file.length(); // Calculate input file size
+        			//System.out.println("File length : " + len); // Print input file size
+        			double framelen = (len / (width * height * 3)); // Calculate input// file's frames
+        			//System.out.println("Numbers of frames : " + framelen); // Print// input// file's// frames
+        			BufferedImage frameA[] = new BufferedImage[(int) framelen]; // Set// the// frame// array
+
+        			byte[] bytes = new byte[(int) len];
+
+        			int offset = 0;
+        			int numRead = 0;
+        			while (offset < bytes.length && (numRead = is.read(bytes, offset, bytes.length - offset)) >= 0) {
+        				offset += numRead;
+        			}
+        			
+        			for (int i = 0; i < framelen; i++) {
+        				BufferedImage img = new BufferedImage(width, height,BufferedImage.TYPE_INT_RGB);
+        				int ind = 0;
+        				for (int y = 0; y < height; y++) {
+        					for (int x = 0; x < width; x++) {
+
+        						byte a = 0;
+        						byte r = bytes[i * width * height * 3 + ind];
+        						byte g = bytes[i * width * height * 3 + ind + height * width];
+        						byte b = bytes[i * width * height * 3 + ind + height * width * 2];
+
+        						int pix = 0xff000000 | ((r & 0xff) << 16) | ((g & 0xff) << 8) | (b & 0xff);
+        						img.setRGB(x, y, pix);
+        						ind++;
+        					}
+        				}
+        				frameA[i] = img;
+        				System.out.println(" width " + i + " : " + frameA[i].getWidth());
+                        System.out.println(" height" + i + " : " + frameA[i].getHeight());
+        			}
+
+                    // you probably want something more involved here
+                    // to display in your UI
+                    System.out.println("image: " + file.getName());
+                    System.out.println(" size  : " + file.length());
+                    imr.display(frameA);
+                } catch (final IOException e) {
+                    /* handle errors here */
+                }
+            }
+            System.out.println("total file in this folder : " + counter);
+        }
+    }
 
 	// Function display the image
 	public void display(BufferedImage[] img) {
